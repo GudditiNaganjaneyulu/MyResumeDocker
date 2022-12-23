c9cf33fdc5122cbdb5429b4d68aa2f8906109089
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2022-12-23 13:30:28 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
  
   
-image="gudditi.jfrog.io/docker/gudditi"  
-  
-#get timestamp for the tag  
-timestamp=$(date +%Y%m%d%H%M%S)  
-  
-tag=$image:$timestamp  
-latest=$image:latest  
-  
-#build image  
-sudo docker build -t $tag . 
+docker build -t gudditi.jfrog.io/docker/gudditi:latest .
 
